e385711db11e395ec87500fbd81a2ad151a75411
Ruggero Querinis <ruggero.querinis@facile.it>
2022-01-03 13:38:59 +0100

    Modificato VatService

--- example/index.php
+++ example/index.php
@@ -1,20 +1,11 @@
 <?php
 
 use partitaiva\PartitaIVA;
+use partitaiva\VatService;
 
 require_once '../vendor/autoload.php';
 
-$pIva = new PartitaIVA('07973780013', 'IT');
-if ($pIva->isValid() === true) {
-    // exist
-}
-
-$pIva = new PartitaIVA('07973780013');
-if ($pIva->isValid() === true) {
-    // exist
-}
-
-$pIva = new PartitaIVA('66780129987', 'FR');
+$pIva = new PartitaIva(new VatService());
 if ($pIva->isValid() === true) {
     // exist
 }
